85de76ac7861aa7a4cc6a3d184f1d02d153f913c
KARANGWA <engineerkarangwa@gmail.com>
2025-08-13 12:39:23 +0200

    login

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://giraffespace-api.urbinaryhub.rw';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://giraffespacev2.onrender.com';
 
 export const API_BASE_URL = `${API_URL}/api/v1`; 
